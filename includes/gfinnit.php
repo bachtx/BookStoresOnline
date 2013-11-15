1947f1f0d2ff0bfa4b9b394ffe5690d0b697ce7f
BachTX <bachtx@smartosc.com>
2013-11-15 10:52:24 +0700

    URL

--- includes/gfinnit.php
+++ includes/gfinnit.php
@@ -4,11 +4,11 @@
 	define('DB_PASSWORD','');
 	define('DB_DATANAME','bookstores');
 	// innit com_language
-	define('ROOTHOST','http://localhost/BookStore_Online/');
+	define('ROOTHOST','http://localhost/BookStoresOnline/');
 	define('URLEDITOR',ROOTHOST.'admincp');
 	define('BASEVIRTUAL0',ROOTHOST.'images/');
 	define('MAX_ROWS_INDEX',40);
-	define('WEBSITE','http://localhost/BookStore_Online/');
+	define('WEBSITE','http://localhost/BookStoresOnline/');
 	
 	define('THUMB_WIDTH',285);
 	define('THUMB_HEIGHT',285);
